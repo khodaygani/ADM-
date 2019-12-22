8aa630b6da6103262acfd61b861a4a39199e9764
DarioneNazionale <56448497+DarioneNazionale@users.noreply.github.com>
2019-12-22 18:39:53 +0100

    Main ready for you

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
-import func_3
-import func_4
-from plottingUtils import plottingOnMaps
+import func_3  # importing functionality 3
+import func_4  # importing functionality 4
+from plottingUtils import plottingOnMaps  # importing the plotting utils
 import sys
 
 if __name__ == '__main__':
 
-    func = int(input("chose your func: "))
+    func = int(input("chose your func: ")) #here the user can chose which functionality wants to run
 
-    if func == 3:
 
-        try:
-            nodes, path = func_3.functionality(1, [3, 2, 1766, 1049903])
+    if func == 1:
+
+        # -------------> insert here your functionality
+
+    elif func == 2:
+
+        # -------------> insert here your functionality
+
+    elif func == 3:
+        # getting the input:
+        start = int(input("insert the starting node ID: "))
+
+        to_visit = list(map(int, input("insert the targets: ").split(" ")))
+
+        try:  # running the functionality
+            nodes, path = func_3.functionality(start, to_visit)
         except Exception:  # if one of those node is not reachable from the start we stop here.
             print("Not possible")
             sys.exit(0)  # stop the program, we don't want to plot.
 
-        plottingOnMaps(nodes, path=path) #  finally plotting!
+        plottingOnMaps(nodes, path=path)  # finally plotting!
 
     elif func == 4:
+        # getting the input:
+        start = int(input("insert the starting node ID: "))
 
-        try:
-            nodes, path = func_4.functionality(1, {3, 1766, 1049903})
-        except Exception: # if one of those node is not reachable from the start we stop here.
+        to_visit = set(map(int, input("insert the targets: ").split(" ")))
+
+        try:  # running the functionality
+            nodes, path = func_4.functionality(start, to_visit)
+        except Exception:  # if one of those node is not reachable from the start we stop here.
             print("Not possible")
-            sys.exit(0) #stop the program, we don't want to plot.
+            sys.exit(0)  # stop the program, we don't want to plot.
 
-        plottingOnMaps(nodes, path=path) #  finally plotting!
+        plottingOnMaps(nodes, path=path)  # finally plotting!
