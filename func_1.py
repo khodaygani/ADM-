8b3be57242809d8310d13b51647b82c9bc6226af
Danieledantoni <56520161+Danieledantoni@users.noreply.github.com>
2019-12-22 19:19:16 +0100

    Update func_1.py

--- func_1.py
+++ func_1.py
@@ -1,25 +1,10 @@
 import utils
 # getting the data...
 utils.prepare_data()
-vertices,distances=utils.get_distance_graph()
-vertices,times=utils.get_time_graph()
-
-# two simple functions to get, from the data, the neigh of a node and the distance between two nodes
-def get_neigh(graph,node):
-    neigh=[]
-    for i in range(len(graph[node])):
-        neigh.append(graph[node][i][0])
-    return neigh
-
-def get_dist(graph,node1,node2):
-    dist=0
-    for i in graph[node1]:
-        if i[0]==node2:
-            dist=dist+i[1]
-        else:
-            dist
-    return dist
-
+from utils import get_neigh
+from utils import get_dist
+from utils import get_distance_path
+from utils import get_path_underd
 
 # this the functionality 1
 # every time you try to run the functionality you have to define the empty list below
@@ -48,27 +33,6 @@ def functionality_1(node_id, graph, d):
     return visited
 
 
-# getting all the path that can be reached from the start-node at distance equal to d (threshold)
-# first of all i build a graph in order to work on it...
-distance = nx.Graph()
-for i in distances:
-    for j in distances[i]:
-        # ...adding nodes...
-        distance.add_node(i)
-
-        # ...the edges and the distance between the nodes in the edge.
-        distance.add_edge(i, j[0], weight=j[1])
-
-
-# simple function to get the distance of a path (given as a list of nodes)
-def get_distance_path(graph, path):
-    distance = 0
-    for i in range(len(path) - 1):
-        if i != path[0]:
-            distance += graph.edges[(path[i], path[i + 1])]['weight']
-    return distance
-
-
 # now i can get all the path whose distance is under the threshold
 def get_path_underd(node_id, graph, dist, d):
     # take all the nodes under the threshold...
@@ -83,3 +47,21 @@ def get_path_underd(node_id, graph, dist, d):
             if get_distance_path(graph, j) <= d and i != node_id:
                 paths.append(j)
     return paths
+def functionality(node_id,distance,d):
+    visited = []
+    dist = {}
+    vertices,distances = utils.get_distance_graph()
+    node_at_dist = functionality_1(node_id,distances,d)
+    
+    # getting all the path that can be reached from the start-node at distance equal to d (threshold)
+    # first of all i build a graph in order to work on it...
+    distance = nx.Graph()
+    for i in distances:
+        for j in distances[i]:
+        # ...adding nodes...
+            distance.add_node(i)
+
+            # ...the edges and the distance between the nodes in the edge.
+            distance.add_edge(i, j[0], weight=j[1])
+    path = get_path_underd(node_id,distance,distances,d)
+    return node_at_dist,path
