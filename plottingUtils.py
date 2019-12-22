51397aa51e4a69f029f4bc3372a9b6e4027c0b98
DarioneNazionale <56448497+DarioneNazionale@users.noreply.github.com>
2019-12-22 15:53:52 +0100

    new version

--- plottingUtils.py
+++ plottingUtils.py
@@ -1,40 +1,56 @@
 import plotly.graph_objects as go
 
 
-def plottingOnMaps(nodes, path=False, paths=False, start=False):
-    fig = go.Figure(go.Scattermapbox())
+def plottingOnMaps(nodes, path=False, paths=False, start=False):  # one function for plot all the functionalities
+    fig = go.Figure(go.Scattermapbox())  # creating the object
 
-    if path:
+    if path:  # we have only one path
+        # gathering the coordinates of all those nodes
         lon, lat = list(zip(*list(map(lambda node: (nodes[node][0] / 1000000, nodes[node][1] / 1000000), path))))
+        # plotting the path
         fig.add_trace(go.Scattermapbox(
             mode="markers+lines",
             lon=lon,
             lat=lat,
             marker={'size': 8, "color": "blue"}))
-
+        # plotting the starting point of the path
         fig.add_trace(go.Scattermapbox(
             mode="markers",
             lon=[nodes[path[0]][0] / 1000000],
             lat=[nodes[path[0]][1] / 1000000],
             marker={'size': 14, "color": "green"}))
 
-    elif paths:
-        for path in paths:
+    elif paths: # if we have several paths
+        for path in paths:  # plotting each path
+            # gathering the coordinates of all those nodes
             lon, lat = list(zip(*list(map(lambda node: (nodes[node][0] / 1000000, nodes[node][1] / 1000000), path))))
+            # plotting the path
             fig.add_trace(go.Scattermapbox(
                 mode="markers+lines",
                 lon=lon,
                 lat=lat,
                 marker={'size': 8, "color": "blue"}))
 
-        if start:
+        if type(start) == list:  # if we have several starting points
+            # gathering the coordinates of all those nodes
+            lon, lat = list(zip(*list(map(lambda node: (nodes[node][0] / 1000000, nodes[node][1] / 1000000), start))))
+            # plotting their points
             fig.add_trace(go.Scattermapbox(
                 mode="markers",
-                lon=[nodes[path[0]][0] / 1000000],
-                lat=[nodes[path[0]][1] / 1000000],
+                lon=lon,
+                lat=lat,
+                marker={'size': 14, "color": "green"}))
+        if type(start) == int:  # if we have only one starting pont for all those paths
+            # plotting the point
+            fig.add_trace(go.Scattermapbox(
+                mode="markers",
+                lon=[nodes[start][0] / 1000000],
+                lat=[nodes[start][1] / 1000000],
                 marker={'size': 14, "color": "green"}))
 
+    # getting the map
     fig.update_layout(mapbox_style="open-street-map")
-    fig.update_layout(margin={'l': 0, 't': 0, 'b': 0, 'r': 0})
+    fig.update_layout(margin={'l': 40, 't': 40, 'b': 40, 'r': 40})
 
+    # plotting finally
     fig.show()
